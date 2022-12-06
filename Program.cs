bd296f72fea07f05a3de58abc73313fa86081d09
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-06 19:06:01 +0300

    Сравнили три двойки чисел

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b >= max)
+
+max = b;
+
+{
+    Console.WriteLine($"Наибольшее число {max}");
+}
+
+
+
